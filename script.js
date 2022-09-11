1fb3d1d3e1289d10401172304b32c2e43653b285
boughris-abdelmalek <boughris@outlook.fr>
2022-09-11 20:16:13 +0100

    final

--- script.js
+++ script.js
@@ -1,28 +1,66 @@
-const playerChoice = 'rock';
+const displayPlayer = document.getElementById("displayPlayer");
+const displayComputer = document.getElementById("displayComputer");
 
-const choices = ['rock', 'paper', 'scissor'];
-const computerChoice = choices[Math.floor(Math.random() * 3)];
+const choice = document.querySelectorAll(".choice");
 
-console.log(computerChoice);
+const playerScore = document.getElementById("playerCount");
+const computerScore = document.getElementById("computerCount");
 
-const game = (player, computer) => {
-    if (player === computer) {
-        console.log('draw');
-    } else {
-        if (player === 'rock' && computer === 'scissor') {
-            console.log('player win');
-        } else if (player === 'paper' && computer === 'rock') {
-            console.log('player win');
-        } else if (player === 'scissor' && computer === 'paper') {
-            console.log('player win');
-        } else if (player === 'rock' && computer === 'paper') {
-            console.log('computer win');
-        } else if (player === 'paper' && computer === 'scissor') {
-            console.log('computer win');
-        } else if (player === 'scissor' && computer === 'rock') {
-            console.log('computer win');
-        }
+const replay = document.getElementById('replay');
+
+const board = document.querySelector('.container');
+const finalScore = document.querySelector('.finalScore-container');
+const winner = document.getElementById('winner');
+
+replay.addEventListener('click', () => {
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    board.style.display = 'flex';
+    finalScore.style.display = 'none';
+})
+
+let userChoice;
+
+choice.forEach((e) => {
+  e.addEventListener("click", (event) => {
+    const choices = {
+      rock: "✊",
+      paper: "✋",
+      scissor: "✌️",
+    };
+    const keys = Object.keys(choices);
+    const computerChoice = keys[Math.floor(Math.random() * 3)];
+    userChoice = event.target.value;
+
+    displayPlayer.textContent = e.textContent;
+    displayComputer.textContent = choices[computerChoice];
+
+    if (computerChoice !== userChoice) {
+      if (userChoice === "rock" && computerChoice === "scissor") {
+        playerScore.textContent++;
+      } else if (userChoice === "paper" && computerChoice === "rock") {
+        playerScore.textContent++;
+      } else if (userChoice === "scissor" && computerChoice === "paper") {
+        playerScore.textContent++;
+      } else if (userChoice === "rock" && computerChoice === "paper") {
+        computerScore.textContent++;
+      } else if (userChoice === "paper" && computerChoice === "scissor") {
+        computerScore.textContent++;
+      } else if (userChoice === "scissor" && computerChoice === "rock") {
+        computerScore.textContent++;
+      }
     }
-}
 
-game(playerChoice, computerChoice);
+    if (computerScore.textContent === '5') {
+        board.style.display = 'none';
+        finalScore.style.display = 'flex';
+        winner.textContent = 'Computer won !';
+    }
+    
+    if (playerScore.textContent === '5') {
+        board.style.display = 'none';
+        finalScore.style.display = 'flex';
+        winner.textContent = 'Player won !';
+    }
+  });
+});
